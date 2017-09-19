409ad0fb835a336041a86728fa403347ec1e4a19
cracker182 <cracker182@outlook.de>
2017-09-19 11:21:40 +0200

    Create passwords.php

---
+++ resources/lang/de-DE/passwords.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Passwörter müssen mindestens 6 Zeichen lang sein und gleich der Bestätigung sein.',
+    'reset' => 'Ihr Passwort wurde zurückgesetzt',
+    'sent' => 'Wir haben Ihnen einen Passwort-Reset-Link via Mail zugesendet.',
+    'token' => 'Das Passwort-Reset-Token ist nicht gültig.',
+    'user' => "Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.",
+];
