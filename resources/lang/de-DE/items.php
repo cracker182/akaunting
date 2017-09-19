4694d1acac90e7df2b06044c6f62b39b1bc8b46b
cracker182 <cracker182@outlook.de>
2017-09-19 11:09:04 +0200

    Create items.php

---
+++ resources/lang/de-DE/items.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'quantities'            => 'Menge|Mengen',
+    'sales_price'           => 'Verkaufspreis',
+    'purchase_price'        => 'Einkaufspreis',
+    'sku'                   => 'SKU',
+];
