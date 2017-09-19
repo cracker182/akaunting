6974672c14bed4155fcf337e5643d1a75fbe0a86
cracker182 <cracker182@outlook.de>
2017-09-19 11:02:12 +0200

    Create header.php

---
+++ resources/lang/de-DE/header.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'change_language'       => 'Sprache Ändern',
+    'last_login'            => 'Letzter Login :time',
+    'notifications' => [
+        'counter'           => '{0} Sie haben keine Benachrichtigungen|{1} Sie haben :count benachrichtigung|[2,*] Sie haben :count Benachrichtigungen',
+        'overdue_invoices'  => '{1} :count überfällige Rechnung|[2,*] :count überfällige Rechungen',
+        'upcoming_bills'    => '{1} :count bevorstehende Ausgangsrechnung|[2,*] :count bevorstehende Ausgangsrechnungen',
+        'view_all'          => 'Alle Anzeigen'
+    ],
+];
