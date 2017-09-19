4fb492cdba8b85f1b0a62ca4ad6d3c8a98ca2577
cracker182 <cracker182@outlook.de>
2017-09-19 10:38:32 +0200

    Create demo.php

---
+++ resources/lang/de-DE/demo.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'accounts_cash'         => 'Geld',
+    'categories_uncat'      => 'unkategorisiert',
+    'categories_deposit'    => 'Deposit',
+    'categories_sales'      => 'Verkauf',
+    'currencies_usd'        => 'US Dollar',
+    'currencies_eur'        => 'Euro',
+    'currencies_gbp'        => 'British Pound',
+    'currencies_try'        => 'Turkish Lira',
+    'taxes_exempt'          => 'Tax Exempt',
+    'taxes_normal'          => 'Normal',
+    'taxes_sales'           => 'Verkaufssteuer',
+];
