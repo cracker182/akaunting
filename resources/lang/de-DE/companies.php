81672c80338e171ffe14792fdeea1d13a57a3a20
cracker182 <cracker182@outlook.de>
2017-09-19 10:33:44 +0200

    Create companies.php

---
+++ resources/lang/de-DE/companies.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'domain'                => 'Domain',
+    'logo'                  => 'Logo',
+    'manage'                => 'Firma verwalten',
+    'all'                   => 'Alle Firmen',
+    'error' => [
+        'delete_active'     => 'Fehler: Aktive Firma kann nicht gelöscht werden. Bitte wechseln Sie erst die Firma!',
+    ],
+];
