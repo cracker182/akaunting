0d9ec43695bad6f1d64dfcad79c0158281af0e46
cracker182 <cracker182@outlook.de>
2017-09-19 11:10:32 +0200

    Create messages.php

---
+++ resources/lang/de-DE/messages.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'success' => [
+        'added'             => ':type hinzugefügt!',
+        'updated'           => ':type aktualisiert!',
+        'deleted'           => ':type gelöscht!',
+    ],
+    'error' => [
+        'not_user_company'  => 'Fehler: Sie haben nicht die nötige Berechtigung um die Firma zu verwalten.',
+    ],
+    'warning' => [
+        'deleted'           => 'Achtung: Sie haben :type nicht gelöscht. Da es :text',
+    ],
+];
